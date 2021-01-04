41461963c6f241b54746947a27ec55ad9b89e22c
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-01-04 12:15:27 -0400

    feat: Add get actors and movies
    Endpoints added:
    	-GET /actors
    	-GET /movies

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ def create_app(test_config=None):
   def index():
     return 'Hello world'
 
+  @app.route('/actors')
+  def get_actors():
+    actors = Actor.query.all()
+
+    actors_list = [actor.format() for actor in actors]
+    return jsonify({
+      'success':True,
+      'actors': actors_list
+    })
+  @app.route('/movies')
+  def get_movies():
+    movies = Movie.query.all()
+
+    movies_list = [movie.format() for movie in movies] 
+    return jsonify({
+      'success':True,
+      'movies':movies_list
+    })
   return app
 
 app = create_app()
